66ef9096342b368b0db0170e6aa22e8c44ed8185
sofiaferreirab <sofiaferreirabezerradasilva571@gmail.com>
2025-05-24 12:38:52 -0300

    Adicionar introdução sobre loops

---
+++ programacao_aulas_sofia/aulas/javascript/loops/loops.js
@@ -0,0 +1,13 @@
+let numero = 50; // 56
+
+
+for (let i = 0; i <= 5; i++) { // i = 6
+  console.log(`i: ${i}`); // i: 5
+  console.log(`número: ${numero}`); // número: 55
+  console.log("\n");
+  numero = numero + 1;
+}
+
+console.log(`número: ${numero}`);
+
+// A operação é executada enquanto a condição for verdadeira
