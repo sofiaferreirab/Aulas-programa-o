8dc0b2e58c166c1353d3528be4963f90010eba78
sofiaferreirab <sofiaferreirabezerradasilva571@gmail.com>
2025-06-14 11:47:27 -0300

    adicionar comentarios explicativos no projeto escola.js

--- programacao_aulas_sofia/aulas/javascript/projetos/escola/escola.js
+++ programacao_aulas_sofia/aulas/javascript/projetos/escola/escola.js
@@ -1,18 +1,25 @@
+// importar bibliota prompt-sync
 const prompt = require('prompt-sync')();
 
+// declaração de variáveis
 let nomeCompleto;
 let genero;
 let idade;
 let serie;
 let enderecoCompleto;
 
+// mensagem inicial
 console.log("Bem-vindo! Cadastre um aluno!");
 console.log("\n");
+
+// formulário para atribuição de valores inseridos pelo usuário às variáveis declaradas anteriormente (usando função pronta da biblioteca prompt-sync)
 nomeCompleto = prompt("Digite o nome completo do aluno: ");
 genero = prompt("Digite o gênero do aluno (Masculino/Feminino): ");
 idade = prompt("Digite a idade do aluno: ");
 serie = prompt("Digite a série do aluno: ");
 enderecoCompleto = prompt("Digite o endereço completo do aluno: ");
+
+// retorno para o usuário
 console.log("\n");
 console.log(`Aluno cadastrado`);
 console.log("\n");
