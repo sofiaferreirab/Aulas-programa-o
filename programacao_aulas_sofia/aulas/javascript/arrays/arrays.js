b6ab6f0375733fd0271d305a31f03dcde36ff5ee
sofiaferreirab <sofiaferreirabezerradasilva571@gmail.com>
2025-06-14 12:34:33 -0300

    introdução arrays

---
+++ programacao_aulas_sofia/aulas/javascript/arrays/arrays.js
@@ -0,0 +1,11 @@
+let nomeCompleto = ["Sofia", "Pedro", "Alice", "Filipe"];
+let genero = ["Feminino", "Masculino"];
+let idade = [16, 12];
+let serie = ["2º ano", "7º ano"];
+let enderecoCompleto = ["Rua machado de Assis, 89, Aguazinha, Olinda - PE", "Rua machado de Assis, 89, Aguazinha, Olinda - PE"];
+
+console.log(`Nome completo: ${nomeCompleto[4]}`);
+nomeCompleto[0] = "Querline";
+console.log(`Nome completo: ${nomeCompleto[4]}`);
+nomeCompleto.pop();
+console.log(`Nome completo: ${nomeCompleto}`);
--- programacao_aulas_sofia/aulas/javascript/projetos/escola/escola.js
+++ programacao_aulas_sofia/aulas/javascript/projetos/escola/escola.js
@@ -1,23 +1,33 @@
 // importar bibliota prompt-sync
 const prompt = require('prompt-sync')();
 
+// string, number, boolean
+
+// array de nomeCompleto, array de genero, array de idade, array de serie, array de enderecoCompleto
+// array ele é basicamente uma "lista" de valores
+
+
 // declaração de variáveis
-let nomeCompleto;
-let genero;
-let idade;
-let serie;
-let enderecoCompleto;
+let nomeCompleto = [];
+let genero = [];
+let idade = [];
+let serie = [];
+let enderecoCompleto = [];
 
 // mensagem inicial
 console.log("Bem-vindo! Cadastre um aluno!");
 console.log("\n");
 
 // formulário para atribuição de valores inseridos pelo usuário às variáveis declaradas anteriormente (usando função pronta da biblioteca prompt-sync)
-nomeCompleto = prompt("Digite o nome completo do aluno: ");
-genero = prompt("Digite o gênero do aluno (Masculino/Feminino): ");
-idade = prompt("Digite a idade do aluno: ");
-serie = prompt("Digite a série do aluno: ");
-enderecoCompleto = prompt("Digite o endereço completo do aluno: ");
+for(i = 0 ; i <= 2 ; i++) {
+  console.log(`Aluno ${i + 1}`)
+  nomeCompleto.push(prompt("Digite o nome completo do aluno: "));
+  genero.push(prompt("Digite o gênero do aluno (Masculino/Feminino): "));
+  idade.push(prompt("Digite a idade do aluno: "));
+  serie.push(prompt("Digite a série do aluno: "));
+  enderecoCompleto.push(prompt("Digite o endereço completo do aluno: "));
+  console.log("\n")
+}
 
 // retorno para o usuário
 console.log("\n");
